63f5456c449d2721a984e53a2f82f7f5d27f7907
Misha Omelyanchum <omelmisha@gmail.com>
2025-08-17 13:09:39 +0300

    upd

--- BACK/themes/Pear/template-parts/single-course-page/single-course-page-editor-section.php
+++ BACK/themes/Pear/template-parts/single-course-page/single-course-page-editor-section.php
@@ -6,8 +6,14 @@ class Single_Course_Page_Editor_Section
     {
         $section_content = get_field('section_content');
         $this->topics = $section_content['topics'];
-        $this->target_group_and_goals = $section_content['target_group_and_goals'];
-        $this->certification = $section_content['certification'];
+        $this->place = $section_content['place'];
+        $this->map_link = $section_content['map_link'];
+        $this->map = $section_content['map'];
+        $this->content = $section_content['content'];
+        $this->certificate = $section_content['certificate'];
+        $this->speaker_image = $section_content['speaker_image'];
+        $this->speaker_title = $section_content['speaker_title'];
+        $this->speaker_text = $section_content['speaker_text'];
     }
 
     public function render()
@@ -35,19 +41,107 @@ class Single_Course_Page_Editor_Section
                             </div>
                         </div>
                     <?php endif; ?>
-                    <?php if (!empty($this->target_group_and_goals)) : ?>
+                    <?php if (!empty($this->content)) : ?>
+                        <?php foreach ($this->content as $item) : ?>
+                            <div class="editor__item">
+                                <div class="editor__title"><?php echo $item['title']; ?></div>
+                                <div class="editor__content">
+                                    <?php echo $item['text']; ?>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+
+                    <?php if (!empty($this->certificate)) : ?>
                         <div class="editor__item">
-                            <div class="editor__title">Zielgruppe und Ziele</div>
-                            <div class="editor__content">
-                                <p><?php echo $this->target_group_and_goals; ?></p>
+                            <div class="editor__title">Zertifikat</div>
+                            <div class="editor__back">
+                                <div class="img">
+                                    <img src="<?= TEMPLATE_PATH ?>/img/content.png" alt="">
+                                </div>
+                                <h2 class="editor__title"><?php echo $this->certificate; ?></h2>
                             </div>
                         </div>
                     <?php endif; ?>
-                    <?php if (!empty($this->certification)) : ?>
+                    <div class="editor__item">
+                        <div class="editor__title">Referent</div>
+                        <div>
+                            <div class="seminar__user">
+                                <?php if (!empty($this->speaker_image)) : ?>
+                                    <div class="img">
+                                        <img src="<?php echo $this->speaker_image; ?>" alt="">
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (!empty($this->speaker_title)) : ?>
+                                    <h3><?php echo $this->speaker_title; ?></h3>
+                                <?php endif; ?>
+                            </div>
+
+                            <?php if (!empty($this->speaker_text)) : ?>
+                                <div class="editor__content">
+                                    <?php echo $this->speaker_text; ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <?php if (!empty($this->map['lat'])) : ?>
                         <div class="editor__item">
-                            <div class="editor__title">Zertifizierung</div>
+                            <div class="editor__title">Veranstaltungsort</div>
                             <div class="editor__content">
-                                <p><?php echo $this->certification; ?></p>
+                                <p><?php echo $this->place; ?></p>
+                                <div class="contact__map editor__map">
+                                    <div class="map__item" id="map"></div>
+
+                                    <script>
+                                        let mapIcon = '<?= TEMPLATE_PATH ?>/img/map-loc.svg';
+
+                                        <?php if (!empty($this->map['lat']) and !empty($this->map['lng'])) : ?>
+                                            let positionMaps = [{
+                                                lat: <?php echo $this->map['lat']; ?>,
+                                                lng: <?php echo $this->map['lng']; ?>,
+                                                text: '<a target="_blank" href="<?php echo $this->map_link; ?>">Explore on google maps</a>'
+                                            }];
+                                        <?php else : ?>
+
+                                            let positionMaps = [{
+                                                lat: 48.5675581,
+                                                lng: 8.1525694,
+                                                text: '<a target="_blank" href="<?php echo $this->map_link; ?>">Explore on google maps</a>'
+                                            }];
+                                            console.log(positionMaps);
+                                        <?php endif; ?>
+                                            (g => {
+                                                var h, a, k, p = "The Google Maps JavaScript API",
+                                                    c = "google",
+                                                    l = "importLibrary",
+                                                    q = "__ib__",
+                                                    m = document,
+                                                    b = window;
+                                                b = b[c] || (b[c] = {});
+                                                var d = b.maps || (b.maps = {}),
+                                                    r = new Set,
+                                                    e = new URLSearchParams,
+                                                    u = () => h || (h = new Promise(async (f, n) => {
+                                                        await (a = m.createElement("script"));
+                                                        e.set("libraries", [...r] + "");
+                                                        for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                                                        e.set("callback", c + ".maps." + q);
+                                                        a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                                                        d[q] = f;
+                                                        a.onerror = () => h = n(Error(p + " could not load."));
+                                                        a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                                                        m.head.append(a)
+                                                    }));
+                                                d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+                                            })({
+                                                key: "AIzaSyCEJB7n28bLX63v_VlYbKj2JZ5iu8PN5iI",
+                                                v: "weekly",
+                                            });
+                                    </script>
+
+
+                                </div>
+
                             </div>
                         </div>
                     <?php endif; ?>
